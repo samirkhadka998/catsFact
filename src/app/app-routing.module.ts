c9150ef713b22cccca86e6b24d6059b0b2b020aa
Samir Khadka <samir.khadka@clinis.com.au>
2023-05-04 12:02:15 +0545

    --added default routing--

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoreComponent } from './bore/bore.component';
+import { CatComponent } from './cat/cat.component';
 
-const routes: Routes = [];
+const routes: Routes = [{component : BoreComponent, path : 'bore'}, {component : CatComponent, path : 'cat'}, { path : '', redirectTo :'/cat',pathMatch : 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
